57f63ffb3e18b1295c877eeb62abadc2ec41a914
Gaurav Singh Sirari <gauravsirari424@gmail.com>
2024-07-09 16:47:42 +0530

    event listner

--- script.js
+++ script.js
@@ -72,40 +72,83 @@
 //cloning
 
 //iteration
-let src1 = {
-  a1 : 10,
-  b1:20,
-  c1:30
-}
+// let src1 = {
+//   a1 : 10,
+//   b1:20,
+//   c1:30
+// }
 
-let dest1 = {}
+// let dest1 = {}
 
-for(let key in src1 ){
-  dest1[key] = src1[key]
-}
+// for(let key in src1 ){
+//   dest1[key] = src1[key]
+// }
  
 // console.log(dest1);
 
 // assign 
-let src2 = {
-  a2 : 10,
-  b2 : 20,
-  c2 : 30
-}
+// let src2 = {
+//   a2 : 10,
+//   b2 : 20,
+//   c2 : 30
+// }
 
-let  dest2 = Object.assign({},src2)
+// let  dest2 = Object.assign({},src2)
 // console.log(dest2);
 
 
 //spread 
 
-let src3 = {
-  a3 : 10,
-  b3 : 20, 
-  c3 : 31
+// let src3 = {
+//   a3 : 10,
+//   b3 : 20, 
+//   c3 : 31
+// }
+
+
+// let dest3 = {...src3};
+// console.log(dest3);
+
+
+
+//template literal
+// console.log(`my name is ${name}`);
+
+
+ //function
+
+//  function countTwoNumber(num1,num2){
+//   return num1+num2;
+
+
+//  }
+//  console.log(countTwoNumber(3,4));
+
+
+
+// const countThreeNumber = (a,b,c)=>{
+// return a+b+c;
+// }
+
+// console.log(countThreeNumber(4,5,6)); 
+
+// sum of arguments 
+// function countfunction (a,b){
+//   let total =0;
+//   for(let value of arguments)
+//   total = total + value;
+// return total;
+// }
+
+
+// console.log(countfunction(1,2,4,4,6,7,6555,));
+
+
+const eventFunction = ()=>{
+  console.log("Event listner");
 }
 
 
-let dest3 = {...src3};
-console.log(dest3);
+document.addEventListener('click',eventFunction);
 
+document.removeEventListener('click',eventFunction);
